783ec020044d2502636930e61b8fb159c30d9484
Hendy Irawan <ceefour666@gmail.com>
2014-12-06 09:49:17 +0000

    Tested working on Android, thanks to khokhani1510 :)
    http://answers.unity3d.com/questions/504707/replace-input-get-axis-horizontal-with-touch-input.html

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	private int count;
+
+	void Start() {
+		count = 0;
+		countText.text = "Count: " + count;
+	}
 
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -12,14 +20,27 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
-	}
 
+		if (Input.touchCount > 0 && 
+		    Input.GetTouch(0).phase == TouchPhase.Moved) {
+			
+			// Get movement of the finger since last frame
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			
+			// Move object across XY plane
+			rigidbody.AddForce (new Vector3(touchDeltaPosition.x, 0.0f, touchDeltaPosition.y)
+			                    * speed * Time.deltaTime);
+		}
+	}
+	
 	void OnTriggerEnter(Collider other) {
 		//Destroy(other.gameObject);
 		// gameObject.tag = "Player";
 		// gameObject.SetActive(false);
 		if (other.gameObject.tag == "PickUp") {
 			other.gameObject.SetActive(false);
+			count++;
+			countText.text = "Count: " + count;
 		}
 	}
 
